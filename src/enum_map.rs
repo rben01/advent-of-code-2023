3dc136bc81458340f34da3a4843b78099af29c27
Robert Bennett <rltbennett@icloud.com>
2023-12-19 18:00:04 -0500

    Manually implemented `Clone` and `Copy` on `EnumMap` so that it wouldn't require the enum to also be `Clone`/`Copy` (which the derived traits would require) Added `Display` impl to `EnumMap`

--- src/enum_map.rs
+++ src/enum_map.rs
@@ -1,16 +1,23 @@
 use std::{
 	array,
+	fmt::{self, Write},
 	marker::PhantomData,
 	ops::{Index, IndexMut},
 };
-
 use strum::IntoEnumIterator;
 
 // We want to add `E: EnumCount` so we can replace `N` with `E::COUNT`, but this
 // requires a highly unstable compiler feature, `generic_const_exprs`. So we're stuck
 // with the somewhat redundant `N`
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 pub(crate) struct EnumMap<const N: usize, E, T>([T; N], PhantomData<E>);
+
+impl<const N: usize, E, T: Copy> Copy for EnumMap<N, E, T> {}
+impl<const N: usize, E, T: Clone> Clone for EnumMap<N, E, T> {
+	fn clone(&self) -> Self {
+		Self(self.0.clone(), PhantomData)
+	}
+}
 
 impl<const N: usize, E, T> EnumMap<N, E, T> {
 	#[allow(dead_code)]
@@ -59,5 +66,23 @@ impl<const N: usize, E: Into<usize>, T: Default> FromIterator<(E, T)> for EnumMa
 			this[idx] = x;
 		}
 		this
+	}
+}
+
+impl<const N: usize, E, T> fmt::Display for EnumMap<N, E, T>
+where
+	T: Copy + fmt::Display,
+{
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		f.write_char('[')?;
+		let mut iter = (*self).into_array().into_iter();
+		if let Some(x) = iter.next() {
+			write!(f, "{x}")?;
+		}
+		for x in iter {
+			write!(f, ", {x}")?;
+		}
+		f.write_char(']')?;
+		Ok(())
 	}
 }
